1b975f2e22ff4ac2fa5febf77ed302093e7c8efe
Sanzhar Beishekeev <franki-55@mail.ru>
2024-11-11 13:40:39 +0600

    Создал форму для создания задачи

---
+++ src/components/TodoForm/TodoForm.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+const TodoForm = () => {
+    const [form, setForm] = useState({
+        title: "",
+    });
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default TodoForm;
