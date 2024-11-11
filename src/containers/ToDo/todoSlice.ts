edc5faea6b9aa9c54ae945d2c989c0fc3c8c4805
Sanzhar Beishekeev <franki-55@mail.ru>
2024-11-11 16:17:33 +0600

    Добавил changeTodo для изменения типа задачи

--- src/containers/ToDo/todoSlice.ts
+++ src/containers/ToDo/todoSlice.ts
@@ -1,5 +1,6 @@
 import {createAsyncThunk, createSlice, PayloadAction,} from "@reduxjs/toolkit";
 import axiosAPI from "../../axiosAPI.tsx";
+import {RootState} from "../../app/store.ts";
 
 interface ITodo {
     title: string,
@@ -21,13 +22,13 @@ const initialState: ITodoProps = {
 
 
 export const fetchTodo = createAsyncThunk('todo/fetchTodo', async () => {
-    const response = await axiosAPI<{[key: string]: ITodo}>('todo.json');
+    const response = await axiosAPI<{ [key: string]: ITodo }>('todo.json');
     return Object.keys(response.data).map((keyAPI) => {
-            return {
-                ...response.data[keyAPI],
-                id: keyAPI,
-            };
-        });
+        return {
+            ...response.data[keyAPI],
+            id: keyAPI,
+        };
+    });
 });
 
 export const addTodo = createAsyncThunk('todo/addTodo', async (title: string) => {
@@ -35,7 +36,17 @@ export const addTodo = createAsyncThunk('todo/addTodo', async (title: string) =>
     return {id: response.data.id + title, title, status: false};
 });
 
+export const changeTodo = createAsyncThunk<{ id: string, status: boolean }, { id: string }, { state: RootState }>('todo/changeTodo', async ({id}, thunkAPI) => {
+    const currentStatusFromState = thunkAPI.getState().todo.todos.find(todo => todo.id === id);
 
+    if (!currentStatusFromState) {
+        return {id, status: false};
+    }
+    const changeStatus = !currentStatusFromState.status;
+    await axiosAPI.put(`todo${id}.json`, {...currentStatusFromState, status: changeStatus});
+
+    return {id, status: changeStatus};
+});
 
 
 export const todoSlice = createSlice({
@@ -63,6 +74,21 @@ export const todoSlice = createSlice({
                 state.todos.push(action.payload);
             })
             .addCase(addTodo.rejected, (state) => {
+                state.loading = false;
+                state.error = true;
+            })
+            .addCase(changeTodo.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(changeTodo.fulfilled , (state, action: PayloadAction<{id: string, status: boolean}>) =>{
+                state.loading = false;
+                const changeTodo = state.todos.find(todo => todo.id === action.payload.id);
+                if (changeTodo) {
+                    changeTodo.status = action.payload.status;
+                }
+            })
+            .addCase(changeTodo.rejected, (state) => {
                 state.loading = false;
                 state.error = true;
             });
